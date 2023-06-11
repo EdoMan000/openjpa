7eb7a23b928cd3f1d4d305c65561b1b9ebffe094
EdoMan000 <manenti000@gmail.com>
2023-06-11 18:08:22 +0200

    done with the first test on CacheMap.java

--- openjpa-kernel/src/test/java/org/apache/openjpa/util/CacheMapPuttingTest.java
+++ openjpa-kernel/src/test/java/org/apache/openjpa/util/CacheMapPuttingTest.java
@@ -10,9 +10,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static org.mockito.Mockito.*;
+
 @RunWith(value = Parameterized.class)
 public class CacheMapPuttingTest {
-    public static final Class<? extends Exception> SUCCESS = null;
     private CacheMap cacheMap;
     /**
      * Category partitioning for key is: <br>
@@ -27,6 +28,7 @@ public class CacheMapPuttingTest {
     private final STATE_OF_KEY stateOfKey;
     private final STATE_OF_VALUE stateOfValue;
     private final Object existingValue;
+    private final boolean isKeyPinned;
 
     private enum STATE_OF_KEY {
         NULL,
@@ -42,6 +44,7 @@ public class CacheMapPuttingTest {
 
     public CacheMapPuttingTest(PutInputTuple putInputTuple) {
         this.stateOfKey = putInputTuple.stateOfKey();
+        this.isKeyPinned = putInputTuple.isKeyPinned();
         this.stateOfValue = putInputTuple.stateOfValue();
         this.existingValue = new Object();
     }
@@ -56,18 +59,26 @@ public class CacheMapPuttingTest {
     @Parameterized.Parameters
     public static Collection<PutInputTuple> getReadInputTuples() {
         List<PutInputTuple> putInputTupleList = new ArrayList<>();
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, STATE_OF_VALUE.VALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, STATE_OF_VALUE.NULL));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, STATE_OF_VALUE.INVALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, STATE_OF_VALUE.VALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, STATE_OF_VALUE.NULL));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, STATE_OF_VALUE.INVALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, STATE_OF_VALUE.VALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, STATE_OF_VALUE.NULL));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, STATE_OF_VALUE.INVALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, STATE_OF_VALUE.VALID));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, STATE_OF_VALUE.NULL));
-        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, STATE_OF_VALUE.INVALID));
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, false, STATE_OF_VALUE.VALID));              //[1]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, false, STATE_OF_VALUE.NULL));               //[2]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NULL, false, STATE_OF_VALUE.INVALID));            //[3]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, false, STATE_OF_VALUE.VALID));          //[4]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, false, STATE_OF_VALUE.NULL));           //[5]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, false, STATE_OF_VALUE.INVALID));        //[6]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, false, STATE_OF_VALUE.VALID));      //[7]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, false, STATE_OF_VALUE.NULL));       //[8]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, false, STATE_OF_VALUE.INVALID));    //[9]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, false, STATE_OF_VALUE.VALID));           //[10]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, false, STATE_OF_VALUE.NULL));            //[11]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.INVALID, false, STATE_OF_VALUE.INVALID));         //[12]
+        //AFTER JACOCO and ba-dua REPORT                                                                           //
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, true, STATE_OF_VALUE.VALID));            //[13]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, true, STATE_OF_VALUE.NULL));             //[14]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.EXISTENT, true, STATE_OF_VALUE.INVALID));          //[15]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, true, STATE_OF_VALUE.VALID));        //[16]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, true, STATE_OF_VALUE.NULL));         //[17]
+        putInputTupleList.add(new PutInputTuple(STATE_OF_KEY.NOT_EXISTENT, true, STATE_OF_VALUE.INVALID));      //[18]
+        //AFTER PIT REPORT -> used mockito verify() to kill some mutations, no need to add tests
 
         return  putInputTupleList;
     }
@@ -75,14 +86,20 @@ public class CacheMapPuttingTest {
     private static final class PutInputTuple {
         private final STATE_OF_KEY stateOfKey;
         private final STATE_OF_VALUE stateOfValue;
+        private final boolean isKeyPinned;
 
         private PutInputTuple(STATE_OF_KEY stateOfKey,
+                              boolean isKeyPinned,
                               STATE_OF_VALUE stateOfValue) {
             this.stateOfKey = stateOfKey;
+            this.isKeyPinned = isKeyPinned;
             this.stateOfValue = stateOfValue;
         }
         public STATE_OF_KEY stateOfKey() {
             return stateOfKey;
+        }
+        public boolean isKeyPinned() {
+            return isKeyPinned;
         }
         public STATE_OF_VALUE stateOfValue() {
             return stateOfValue;
@@ -91,7 +108,7 @@ public class CacheMapPuttingTest {
 
     @Before
     public void setUpEachTime() {
-        this.cacheMap = new CacheMap();
+        this.cacheMap = spy(new CacheMap());
         switch (stateOfKey){
             case NULL:
                 this.key = null;
@@ -101,10 +118,16 @@ public class CacheMapPuttingTest {
                 break;
             case EXISTENT:
                 this.key = new Object();
-                cacheMap.put(this.key, this.existingValue);
+                this.cacheMap.put(this.key, this.existingValue);
+                if(this.isKeyPinned) {
+                    this.cacheMap.pin(key);
+                }
                 break;
             case NOT_EXISTENT:
                 this.key = new Object();
+                if(this.isKeyPinned) {
+                    this.cacheMap.pin(key);
+                }
                 break;
         }
         switch (stateOfValue){
@@ -123,7 +146,31 @@ public class CacheMapPuttingTest {
     @Test
     public void put() {
         Object retVal = this.cacheMap.put(this.key, this.value);
-        Assert.assertEquals(this.value, this.cacheMap.get(this.key));
+        if(this.stateOfKey == STATE_OF_KEY.NOT_EXISTENT){
+            verify(this.cacheMap).entryAdded(this.key, this.value);
+        } else if (this.stateOfKey == STATE_OF_KEY.EXISTENT) {
+            verify(this.cacheMap).entryRemoved(this.key, this.existingValue, false);
+            verify(this.cacheMap).entryAdded(this.key, this.value);
+        }
+        if(this.isKeyPinned){
+            if(this.stateOfKey == STATE_OF_KEY.NOT_EXISTENT){
+                verify(this.cacheMap, times(2)).writeLock();
+                verify(this.cacheMap, times(2)).writeUnlock();
+            } else if (this.stateOfKey == STATE_OF_KEY.EXISTENT) {
+                verify(this.cacheMap, times(3)).writeLock();
+                verify(this.cacheMap, times(3)).writeUnlock();
+            }
+            Assert.assertEquals(this.value, this.cacheMap.pinnedMap.get(this.key));
+        }else {
+            if(this.stateOfKey == STATE_OF_KEY.NOT_EXISTENT){
+                verify(this.cacheMap).writeLock();
+                verify(this.cacheMap).writeUnlock();
+            } else if (this.stateOfKey == STATE_OF_KEY.EXISTENT) {
+                verify(this.cacheMap, times(2)).writeLock();
+                verify(this.cacheMap, times(2)).writeUnlock();
+            }
+            Assert.assertEquals(this.value, this.cacheMap.cacheMap.get(this.key));
+        }
         if(this.stateOfKey == STATE_OF_KEY.EXISTENT){
             Assert.assertEquals(this.existingValue, retVal);
         }
